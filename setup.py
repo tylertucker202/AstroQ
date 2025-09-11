eb3e08230f8a9999f9b52df15a5b3eb5825c1df6
Tyler Tucker <tylertucker202@gmail.com>
2025-09-10 14:48:06 -1000

    astroq pip install

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_property(prop, project):
     return result.group(1)
 
 setup(
-    name="astroq",
+    name="tcoda-astroq",
     version=get_property('__version__', 'astroq'),
     packages=find_packages(),
     install_requires=[],
