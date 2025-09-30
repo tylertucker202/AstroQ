7bf8d277f5c6eea93b8f00b35db95a97276c8c39
Tyler Tucker <tylertucker202@gmail.com>
2025-09-29 14:40:39 -1000

    use get not getstring

--- astroq/webapp.py
+++ astroq/webapp.py
@@ -40,9 +40,9 @@ cf = './config_template.ini'
 config = ConfigParser()
 config.read(cf)
 
-DATE_TIME_FORMAT = config.getstring('webapp', 'date_time_format', fallback="%Y-%m-%dT%H:%M:%S.%f") 
+DATE_TIME_FORMAT = config.get('webapp', 'date_time_format', fallback="%Y-%m-%dT%H:%M:%S.%f") 
 BANDS = config.getlist('webapp', 'bands', fallback=['band1', 'band3'])
-UPTREE_PATH = config.getstring('webapp', 'uptree_path', fallback='.')
+UPTREE_PATH = config.get('webapp', 'uptree_path', fallback='.')
 ADMIN_IDS = config.getlist('webapp', 'admin', fallback=[])
 
 def load_data_for_path(semester_code, date, band, page=None):
